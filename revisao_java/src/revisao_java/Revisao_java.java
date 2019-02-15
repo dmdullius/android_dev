339a58814515948cfc9273ca3bba7c08b1def0a5
dmdullius <41974502+dmdullius@users.noreply.github.com>
2019-02-14 22:07:47 -0200

    commit aula 01

--- revisao_java/src/revisao_java/Revisao_java.java
+++ revisao_java/src/revisao_java/Revisao_java.java
@@ -1,21 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package revisao_java;
 
-/**
- *
- * @author dmdullius
- */
 public class Revisao_java {
 
-    /**
-     * @param args the command line arguments
-     */
+    //metodo de inicializacao da classe
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Método Main");
+        //declarar variavel do tipo inteiro
+        int soma;
+
+        int multiplica = multiplicaNumeros(2, 3);
+
+        int subtrai = subtraiNumeros(2, 3);
+
+        int divide = divideNumeros(4, 5);
+
+        //atribuir o valor retornado
+        soma = somarNumeros(2, 3);
+
+        multiplica = multiplicaNumeros(2, 3);
+
+        subtrai = subtraiNumeros(2, 3);
+
+        divide = divideNumeros(4, 5);
+
+        System.out.println("soma = " + soma);
+
+        System.out.println("subtração = " + subtrai);
+
+        System.out.println("multiplicação = " + multiplica);
+
+        System.out.println("divisão = " + divide);
+
     }
-    
+
+    private static int somarNumeros(int valor1, int valor2) {
+
+        int resultado;
+        resultado = valor1 + valor2;
+        return resultado;
+    }
+
+    private static int subtraiNumeros(int valor1, int valor2) {
+
+        int resultado;
+        resultado = valor1 - valor2;
+        return resultado;
+    }
+
+    private static int multiplicaNumeros(int valor1, int valor2) {
+
+        int resultado;
+        resultado = valor1 * valor2;
+        return resultado;
+    }
+
+    private static int divideNumeros(int valor1, int valor2) {
+
+        int resultado;
+        resultado = valor1 / valor2;
+        return resultado;
+    }
 }
