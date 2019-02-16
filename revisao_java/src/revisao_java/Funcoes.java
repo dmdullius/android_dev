5e0c6b6486355779421c9bf35892954bf1f8393c
douglas <douglas@ORION-DESKTOP-01.local>
2019-02-16 10:09:34 -0200

    projeto finalizado, faltanto algumas correções, incluido nova funcionalidade(alterar senha) Signed-off-by:douglas <douglas@ORION-DESKTOP-01.local>

---
+++ revisao_java/src/revisao_java/Funcoes.java
@@ -0,0 +1,34 @@
+
+package revisao_java;
+
+
+public class Funcoes {
+     public static int somarNumeros(int valor1, int valor2) {
+
+        int resultado;
+        resultado = valor1 + valor2;
+        return resultado;
+    }
+
+    public  int subtraiNumeros(int valor1, int valor2) {
+
+        int resultado;
+        resultado = valor1 - valor2;
+        return resultado;
+    }
+
+    public  int multiplicaNumeros(int valor1, int valor2) {
+
+        int resultado;
+        resultado = valor1 * valor2;
+        return resultado;
+    }
+
+    public  int divideNumeros(int valor1, int valor2) {
+
+        int resultado;
+        resultado = valor1 / valor2;
+        return resultado;
+    }
+}
+
