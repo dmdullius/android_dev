2f30dbc689ab3b72bfd0cdc23ad6cf7453ac2800
douglas <douglas@ORION-DESKTOP-01.local>
2019-02-22 22:57:02 -0300

    SEGUNDO EXERCICIO CONCLUIDO Signed-off-by:douglas <douglas@ORION-DESKTOP-01.local>

--- revisao_java/src/revisao_java/Funcoes.java
+++ revisao_java/src/revisao_java/Funcoes.java
@@ -1,7 +1,4 @@
-
 package revisao_java;
-
-
 
 public class Funcoes {
 
@@ -11,20 +8,22 @@ public class Funcoes {
         resultado = valor1 + valor2;
         return resultado;
     }
-   static int [] num={11,3,12,4,50,60,70};
-  public static int somarNumeros_a(int [] numeros) {
+    static int[] num = {1,2,3,4,5,6,7};
 
-        int  resultado = 0;
+    public static int somarNumeros_a(int[] numeros) {
 
-         
-     for(int i = 0; i<num.length;i++){
-			resultado = num[i]+num[i]+num[i]+num[i]+num[i]+num[i]+num[i];
-			
-		}
-           System.out.println(" O RESULTADO DA SOMA DO ARRAY É:  " +resultado);
-           return resultado;
-       }        
-    
+        int resultado = 0;
+
+        for (int i = 0; i < num.length; i++) {
+
+          
+                resultado = num[0]+num[1]+num[2]+num[3]+num[4]+num[5]+num[6]; 
+            
+        }
+        System.out.println(" O RESULTADO DA SOMA DO ARRAY É:  " + resultado);
+        return resultado;
+    }
+
     public int subtraiNumeros(int valor1, int valor2) {
 
         int resultado;
@@ -39,6 +38,19 @@ public class Funcoes {
         return resultado;
     }
 
+    public static int multiplicaNumeros_a(int[] numeros) {
+        int resultado = 0;
+
+        for (int i = 0; i < num.length; i++) {
+           
+                resultado = num[0]*num[1]*num[2]*num[3]*num[4]*num[5]*num[6]; 
+
+            
+        }
+        System.out.println(" O RESULTADO DA MULTIPLICAÇÃO DO ARRAY É:  " + resultado);
+        return resultado;
+    }
+
     public int divideNumeros(int valor1, int valor2) {
 
         int resultado;
@@ -46,8 +58,3 @@ public class Funcoes {
         return resultado;
     }
 }
-
-
-
-
-
