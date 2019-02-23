21ab73039220a451ac437eebc570b2a1b2663056
douglas <douglas@ORION-DESKTOP-01.local>
2019-02-22 22:24:29 -0300

    PRIMEIRO EXERCICIO CONCLUIDO Signed-off-by:douglas <douglas@ORION-DESKTOP-01.local>

--- revisao_java/src/revisao_java/Funcoes.java
+++ revisao_java/src/revisao_java/Funcoes.java
@@ -1,6 +1,8 @@
 
 package revisao_java;
 
+
+
 public class Funcoes {
 
     public static int somarNumeros(int valor1, int valor2) {
@@ -9,7 +11,20 @@ public class Funcoes {
         resultado = valor1 + valor2;
         return resultado;
     }
+   static int [] num={11,3,12,4,50,60,70};
+  public static int somarNumeros_a(int [] numeros) {
 
+        int  resultado = 0;
+
+         
+     for(int i = 0; i<num.length;i++){
+			resultado = num[i]+num[i]+num[i]+num[i]+num[i]+num[i]+num[i];
+			
+		}
+           System.out.println(" O RESULTADO DA SOMA DO ARRAY É:  " +resultado);
+           return resultado;
+       }        
+    
     public int subtraiNumeros(int valor1, int valor2) {
 
         int resultado;
@@ -30,9 +45,7 @@ public class Funcoes {
         resultado = valor1 / valor2;
         return resultado;
     }
-
 }
-
 
 
 
