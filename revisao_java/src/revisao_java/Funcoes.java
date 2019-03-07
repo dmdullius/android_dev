36c1c982815408160c06f420271f881e4889945d
douglas <douglas@ORION-DESKTOP-01.local>
2019-03-06 23:08:28 -0300

    TERCEIRO EXERCICIO CONCLUIDO( FEITAS AS DEVIDAS CORRECOES) Signed-off-by:douglas <douglas@ORION-DESKTOP-01.local>

--- revisao_java/src/revisao_java/Funcoes.java
+++ revisao_java/src/revisao_java/Funcoes.java
@@ -8,7 +8,8 @@ public class Funcoes {
         resultado = valor1 + valor2;
         return resultado;
     }
-       public int subtraiNumeros(int valor1, int valor2) {
+
+    public int subtraiNumeros(int valor1, int valor2) {
 
         int resultado;
         resultado = valor1 - valor2;
@@ -22,19 +23,17 @@ public class Funcoes {
         return resultado;
     }
 
-  
-
     public int divideNumeros(int valor1, int valor2) {
 
         int resultado;
         resultado = valor1 / valor2;
         return resultado;
     }
-    
+
 //--------------------------------------------REVISÃO ARRAY + FOR-----------------------------------------------------------------
-    
-    static int[] num = {3,2,3,5};
-    
+    static int[] num = {3, 2, 3, 5};
+    static int[] mega_sena_a;
+
     public static int somarNumeros_a(int[] numeros) {
 
         int resultado = 0;
@@ -57,37 +56,48 @@ public class Funcoes {
             resultado *= var;
 
         }
-        
-               
 
         System.out.println(" O RESULTADO DA MULTIPLICAÇÃO DO ARRAY É:  " + resultado);
         return resultado;
     }
 
- 
-    public static double divideNumeros(int [] numeros){
-            double resultado = num[0];
-            for (int i = 1; i<num.length; i ++){
-                resultado /= num [i];
-            }
-            
-    
-    return resultado;
+    public static double divideNumeros(int[] numeros) {
+        double resultado = num[0];
+        for (int i = 1; i < num.length; i++) {
+            resultado /= num[i];
         }
-    
- public static int subtraiNumeros_a(int [] numeros){
-     int resultado = 1;
-     for (int i = 0; i<num.length; i ++){
-                resultado -= num [i];
-    
-             
- }
-            
-  return resultado;
- }
- 
- 
-}
 
- 
+        return resultado;
+    }
+
+    public static int subtraiNumeros_a(int[] numeros) {
+        int resultado = 1;
+        for (int i = 0; i < num.length; i++) {
+            resultado -= num[i];
+
+        }
+
+        return resultado;
+    }
+
+    public static int mega_Sena(int[] mega_sena) {
+        double numeros_sorteados = 0;
+        mega_sena_a = new int[6];
+
+        for (int i = 0; i < mega_sena_a.length; i++) {
+
+            numeros_sorteados = (Math.random() * 61)+1;
+            
+            
+                mega_sena_a[i] = (int) numeros_sorteados;
+            System.out.println("Numeros Sorteados: " +(int)numeros_sorteados);
+            
+
+                
+
+        }
+        return (int) numeros_sorteados;
+    }
+}
+    
 
